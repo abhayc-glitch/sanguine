388351175887d00980432f63e3d3147b218022f4
abhayc-glitch <78511893+abhayc-glitch@users.noreply.github.com>
2021-08-17 15:48:59 -0700

    Created a HttpVerticle for proper server start

---
+++ backend/src/main/java/com/server/backend/HttpVerticle.java
@@ -0,0 +1,48 @@
+package com.server.backend;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.core.json.Json;
+import io.vertx.ext.web.Route;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.handler.BodyHandler;
+
+
+
+public class HttpVerticle extends AbstractVerticle {
+  @Override
+  public void start(Promise<Void> startPromise) {
+
+    // create a apiRouter to handle the API
+    // create a base router to handle everything at the root level
+    Router baseRouter = Router.router(vertx);
+    Router apiRouter = Router.router(vertx);
+
+    baseRouter.route("/").handler(routingContext -> {
+      HttpServerResponse response = routingContext.response();
+      response.putHeader("content-type", "text/plain").end("Hello Vert.x!");
+    });
+
+    apiRouter.route("/user*").handler(BodyHandler.create());
+    apiRouter.post("/users").handler(this::registerUser);
+    // We are going to build out the register method below
+    baseRouter.mountSubRouter("/api", apiRouter);
+
+    HttpServer listen = vertx.createHttpServer()
+      .requestHandler(baseRouter)
+      .listen(8080, result -> {
+        if (result.succeeded()) {
+          startPromise.complete();
+        } else {
+          startPromise.fail(result.cause());
+        }
+      });
+
+
+  }
+  private void registerUser(RoutingContext routingContext) {}
+
+}
--- backend/src/main/java/com/server/backend/MainVerticle.java
+++ backend/src/main/java/com/server/backend/MainVerticle.java
@@ -1,41 +1,16 @@
 package com.server.backend;
 
+
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
 
 public class MainVerticle extends AbstractVerticle {
+
   @Override
-  public void start() throws Exception {
-    // Create a Router
-    Router router = Router.router(vertx);
+  public void start(Promise<Void> startPromise) {
 
-    // Mount the handler for all incoming requests at every path and HTTP method
-    router.route().handler(context -> {
-      // Get the address of the request
-      String address = context.request().connection().remoteAddress().toString();
-      // Get the query parameter "name"
-      MultiMap queryParams = context.queryParams();
-      String name = queryParams.contains("name") ? queryParams.get("name") : "unknown";
-      // Write a json response
-      context.json(
-        new JsonObject()
-          .put("name", name)
-          .put("address", address)
-          .put("message", "Hello " + name + " connected from " + address)
-      );
-    });
-
-    // Create the HTTP server
-    vertx.createHttpServer()
-      // Handle every request using the router
-      .requestHandler(router)
-      // Start listening
-      .listen(8888)
-      // Print the port
-      .onSuccess(server ->
-        System.out.println(
-          "HTTP server started on port " + server.actualPort()
-        )
-      );
+    vertx.deployVerticle(HttpVerticle.class.getName());
+    startPromise.complete();
   }
+
 }
