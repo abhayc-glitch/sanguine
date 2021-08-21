38be1d74f48b4277f42f76a5a72e1f6ae7799e40
abhayc-glitch <78511893+abhayc-glitch@users.noreply.github.com>
2021-08-20 20:02:45 -0700

    Created a User Model with Attributes

---
+++ backend/src/main/java/com/server/backend/models/User.java
@@ -0,0 +1,61 @@
+package com.server.backend.models;
+
+import io.vertx.core.json.JsonObject;
+
+public class User {
+  // Nickname
+  // Password
+  // ID
+  // email
+  // Token
+  String nickname;
+
+  String password;
+
+  int id;
+
+  String email;
+
+  String token;
+
+  public User(String nickname, String password, int id, String email, String token){
+    this.nickname = nickname;
+    this.password = password;
+    this.id = id;
+    this.email = email;
+    this.token = token;
+  }
+  // Json Object for communicating serializing and transmitting structured data over a network connection.
+  public JsonObject toluminJson(){
+    JsonObject retVal = new JsonObject();
+    JsonObject user = new JsonObject()
+      .put("nickname", this.nickname)
+      .put("email", this.email)
+      .put("token", this.token)
+      .put("id", this.id);
+    retVal.put("user", user);
+    return retVal;
+  }
+  // Getters and Setters
+
+  public String getNickname(){
+    return nickname;
+  }
+
+  public void setNickname(String nickname){
+    nickname = this.nickname;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password){
+    password = this.password;
+  }
+
+
+
+
+
+}
