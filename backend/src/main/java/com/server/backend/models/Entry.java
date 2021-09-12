4df2e47b8e5517b6eac868251742e62980bcd4be
abhayc-glitch <78511893+abhayc-glitch@users.noreply.github.com>
2021-09-12 12:38:08 -0700

    Created a Entity for Entry

--- backend/src/main/java/com/server/backend/models/Entry.java
+++ backend/src/main/java/com/server/backend/models/Entry.java
@@ -1,6 +1,67 @@
 package com.server.backend.models;
 
-public class Entry {
-   // Discuss what we need to input such as mood, categories etc
+import java.time.Instant;
 
+public class Entry {
+
+  // An Entry is where the user can do a check in, and describe there feelings Similar ot a post.java
+
+  private String title;
+
+  private String textContent;
+
+  private Long postId;
+
+  private int mood;
+
+  private Instant createdDate;
+
+
+  public Entry(String title, String textContent, Long postId, int mood, Instant createdDate) {
+    this.title = title;
+    this.textContent = textContent;
+    this.postId = postId;
+    this.mood = mood;
+    this.createdDate = createdDate;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    title = this.title;
+  }
+
+  public String getTextContent() {
+    return textContent;
+  }
+
+  public void setTextContent(String textContent) {
+    this.textContent = textContent;
+  }
+
+  public Long getPostId() {
+    return postId;
+  }
+
+  public void setPostId(Long postId) {
+    this.postId = postId;
+  }
+
+  public int getMood() {
+    return mood;
+  }
+
+  public void setMood(int mood) {
+    this.mood = mood;
+  }
+
+  public Instant getCreatedDate() {
+    return createdDate;
+  }
+
+  public void setCreatedDate(Instant createdDate) {
+    this.createdDate = createdDate;
+  }
 }
--- backend/src/main/java/com/server/backend/models/Quote.java
+++ backend/src/main/java/com/server/backend/models/Quote.java
@@ -1,4 +1,64 @@
 package com.server.backend.models;
 
+
+// CHANGE THISSSSSSSSSSssssssssšsssss
 public class Quote {
+  // Nickname
+  // Password
+  // ID
+  // email
+  // Token
+  String nickname;
+
+  String password;
+
+  String id;
+
+  String email;
+
+  String token;
+
+  public Quote(String nickname, String password, String id, String email, String token) {
+    this.nickname = nickname;
+    this.password = password;
+    this.email = email;
+    this.token = token;
+  }
+
+  // Json Object for communicating serializing and transmitting structured data
+  // over a network connection
+  // Getters and Setters
+
+  public String getNickname() {
+    return nickname;
+  }
+
+  public void setNickname(String nickname) {
+    nickname = this.nickname;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    password = this.password;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    email = this.email;
+  }
+
+  public String getToken() {
+    return token;
+  }
+
+  public void setToken(String token) {
+    token = this.token;
+  }
+
 }
