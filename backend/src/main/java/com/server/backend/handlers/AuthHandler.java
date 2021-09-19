81b5ddf5ce71b34c31697758f8a80505f6c3b563
abhayc-glitch <78511893+abhayc-glitch@users.noreply.github.com>
2021-09-19 12:24:10 -0700

    Implemented a Handling System

---
+++ backend/src/main/java/com/server/backend/handlers/AuthHandler.java
@@ -0,0 +1,16 @@
+package com.server.backend.handlers;
+
+
+import io.vertx.core.impl.logging.Logger;
+import io.vertx.core.impl.logging.LoggerFactory;
+import io.vertx.reactivex.core.Vertx;
+
+// Class to handle Auth Requests
+public class AuthHandler extends BaseHandler{
+  // Create a Logger for the CLI
+  protected static Logger logger = LoggerFactory.getLogger(AuthHandler.class);
+
+  public AuthHandler(Vertx vertx) {
+    super(vertx);
+  }
+}
