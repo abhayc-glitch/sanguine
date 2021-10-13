03f1231561b6d27fa1528b3559e8b9941c0cc05b
abhayc-glitch <78511893+abhayc-glitch@users.noreply.github.com>
2021-10-13 13:28:09 -0700

    Added authentication managers and password encoding.

--- server/src/main/java/com/backend/server/config/SecurityConfig.java
+++ server/src/main/java/com/backend/server/config/SecurityConfig.java
@@ -1,9 +1,49 @@
 package com.backend.server.config;
 
+import com.backend.server.service.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-        
+    
+    @Autowired
+    private UserService userService;
+    // Helps with JDBC and Auth security
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService);
+    }
+    // Helps fix the Common HTTP security vunerabilities
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests()
+        .antMatchers("/subs", "/auth")
+        .permitAll()
+        .anyRequest()
+        .authenticated();
+    }
+    // Encodes and Encrypts Passwords
+    @Bean 
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+    // Process an Authentication Request
+    // @Bean means its an OBJECT like class
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
 }
